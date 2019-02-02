5108009a2385a457ba11d7b78951977cadffb031
Harman Bains <bains.hmn@gmail.com>
2019-02-01 17:45:39 -0800

    Component creation and organization

---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Harman Bains</h1>
+      <p>Im an explorer trying to fix my ship and go exploring some more!</p>
+      <ul>
+        <li>Big Sur</li>
+        <li>Norway</li>
+        <li>New Zealand</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MainContent
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <navigation>
+      <p>ShitNav</p>
+    </navigation>
+  )
+}
+
+export default NavBar
