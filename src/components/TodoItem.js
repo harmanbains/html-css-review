ea3028f0f2cd3d7968b73d5cf8b7dd1231dbfeec
Harman Bains <bains.hmn@gmail.com>
2019-02-16 21:29:49 -0800

    added different styling for completed items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ import './TodoItem.css';
 
 function TodoItem(props) {
   return (
-    <div className="todo-item" >
+    <div className={props.item.completed ? "todo-item completed" : "todo-item"} >
       <input type="checkbox" name="todo" checked={props.item.completed} onChange={props.onChange}/>
       <p>{props.item.text}</p>
     </div>
